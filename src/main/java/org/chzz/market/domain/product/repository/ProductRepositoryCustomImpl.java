7527a6fbedcf99cd7615a58734790b8352e273ce
junest66 <chlwndks333@gmail.com>
2024-10-04 23:03:46 +0900

    fix: 내가 등록한 사전 등록 목록 조회 쿼리 수정
    CHZZ-123

--- src/main/java/org/chzz/market/domain/product/repository/ProductRepositoryCustomImpl.java
+++ src/main/java/org/chzz/market/domain/product/repository/ProductRepositoryCustomImpl.java
@@ -136,8 +136,9 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
     public Page<ProductResponse> findProductsByUserId(Long userId, Pageable pageable) {
         JPAQuery<?> baseQuery = jpaQueryFactory.from(product)
                 .join(product.user, user)
-                .join(auction.product, product)
-                .on(user.id.eq(product.id));
+                .leftJoin(auction).on(auction.product.eq(product))
+                .leftJoin(like).on(like.product.eq(product).and(like.user.id.eq(userId)))
+                .where(auction.isNull().and(user.id.eq(userId)));
 
         return getProductResponses(pageable, baseQuery);
     }
