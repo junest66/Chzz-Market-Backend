1c7c9942ba589590e1b1671cc4cee319714c0dc1
Jun Choi <121853214+junest66@users.noreply.github.com>
2024-11-26 18:39:06 +0900

    fix: 경매 종료 스케줄링 의존성 주입 문제 수정 (#134)

--- src/main/java/org/chzz/market/domain/auction/schedule/AuctionEndJob.java
+++ src/main/java/org/chzz/market/domain/auction/schedule/AuctionEndJob.java
@@ -1,18 +1,18 @@
 package org.chzz.market.domain.auction.schedule;
 
-import lombok.RequiredArgsConstructor;
 import org.chzz.market.domain.auction.service.AuctionEndService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * 경매 스케줄링 종료 작업
  */
 @Component
-@RequiredArgsConstructor
 public class AuctionEndJob implements Job {
-    private final AuctionEndService auctionEndService;
+    @Autowired
+    private AuctionEndService auctionEndService;
 
     @Override
     public void execute(JobExecutionContext context) {
